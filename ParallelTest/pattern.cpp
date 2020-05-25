992d5d1755258ecab958316c4a7873aa8f4c16ad
frchick <frchick.00@gmail.com>
2020-05-25 22:32:44 +0900

    パパの実装でほぼ置き換え。

--- ParallelTest/pattern.cpp
+++ ParallelTest/pattern.cpp
@@ -43,8 +43,8 @@ int fingCollisionPairSub(int N, int **CollisionPair, int k, int pairNo);
 
 int main(void)
 {
-	// チェック用の変数
-	const int numBalls = 8; // 偶数のみ
+	// コリジョンチェックを行うボールの数
+	const int numBalls = 16; // 偶数のみ
 
 	// コリジョン判定を行う2個のボールのペアを、二次元配列のグリッドで表す。
 	// 変数内の数字は、何回目の並列処理グループで判定するか。
@@ -130,16 +130,22 @@ int main(void)
 		cout << "成功！" << endl;
 	}
 	// グリッド表示(縦横ヘッダ付き)
-	cout << "   ";
+	cout << "  |";
 	for (int x = 0; x < numBalls; x++)
 	{
 		cout << ato2i(x) << ".";
 	}
 	cout << endl;
+	cout << "--+";
+	for (int x = 0; x < numBalls; x++)
+	{
+		cout << "---";
+	}
+	cout << endl;
 
 	for (int y = 0; y < numBalls; y++)
 	{
-		cout << ato2i(y) << ":";
+		cout << ato2i(y) << "|";
 
 		for (int x = 0; x < numBalls; x++)
 		{
@@ -192,57 +198,71 @@ int Wave(int L, int i)
 int fingCollisionPairSub(int N, int **CollisionPair, int k, int pairNoBase)
 {
 	// 一回一次元上に順番を入れてから表に代入する
-	int *pair;
-	pair = new int[N];
+	int *pair = new int[N];
 	// 空にする(-1)
 	for (int i = 0; i < N; i++)
 	{
 		pair[i] = -1;
 	}
 
-	int maxVal = k;
-
 	// 値を入れていく
-	int p = 0;
-	bool isPairFull = false;
-	while(isPairFull == false)
+	while(true)
 	{
-		int maxK = 0;
-		int x = p;
-		for (int i = 0; i < N / 2; i++)
+		// チェックをスタートするボールの組み合わせを見つける
+		int groupID = pairNoBase;
+		int p;
+		for (p = 0; p < N; p++)
 		{
-			// 今から入れようとする値
-			int kk = Wave(1, i);
-			// 次のチェック
-			int Nx;
-			Nx = x + k;
-			if (N <= Nx) Nx = Nx - N;
-
-			if (pair[Nx] != kk)
+			// 未チェックのペアからスタート
+			if (pair[p] == -1)
 			{
-				pair[x] = kk;
-				if (maxK < kk) maxK = kk;
+				// このペアに含まれるボールが参照される別のペアが未チェック、
+				// もしくは別の並列処理グループならOK。
+				// そうでないなら別の並列処理グループでスタートする。
+				int pn = p - k;
+				if (pn < 0) pn = pn + N;
+				int pp = p + k;
+				if (N <= pp) pp = pp - N;
+				while (true)
+				{
+					if ((/*(pair[pn] == -1) || */(pair[pn] != groupID)) &&
+						(/*(pair[pp] == -1) || */(pair[pp] != groupID)))
+					{
+						break;
+					}
+					groupID++;
+				}
+				break;
+			}
+		}
+		// 全てのペアをチェック済みなら完了
+		if (p == N) break;
+
+		for (int i = 0; i < N; i++)
+		{
+			// 今からコリジョンチェックを含めようとする、並列処理グループの番号
+			int gid = groupID + Wave(1, i);
+			// このペアに含まれるボールが参照される次のペアが未チェック、
+			// もしくは別の並列処理グループならOK。
+			int pp = p + k;
+			if (N <= pp) pp = pp - N;
+
+			if ((pair[p] == -1) && (pair[pp] != gid))
+			{
+				pair[p] = gid;
 			}
 			else
 			{
-				pair[x] = maxK + 1;
-				maxK = maxK + 1;
+				// ダメだった場合には、このスキャンを終了して、次にスタート可能なペアを探し直す。
 				break;
 			}
-			x = Nx;
-		}
-		// 空きがないかチェック
-		isPairFull = true;
-		for (p = 0; p < N; p++)
-		{
-			if (pair[p] == -1)
-			{
-				isPairFull = false;
-				break;
-			}
+			// このペアに含まれつボールが参照される次のペアへ進む。
+			p = pp;
 		}
 	}
+
 	// xからyを求め、そこに一時配列の情報を入れていく
+	int maxGroupID = 0;
 	int x = k - 1, y = 0;
 	for (int i = 0; i < N; i++)
 	{
@@ -256,9 +276,15 @@ int fingCollisionPairSub(int N, int **CollisionPair, int k, int pairNoBase)
 			y = x - k;
 		}
 
-		CollisionPair[y][x] = pair[i] + pairNoBase;
-		if (maxVal < pair[i] + pairNoBase) maxVal = pair[i] + pairNoBase;
+		// 折り返した組み合わせがすでに別の並列処理グループに入っている可能性もある。
+		// そのような場合は上書きしない。
+		if (CollisionPair[y][x] < 0)
+		{
+			CollisionPair[y][x] = pair[i];
+			if (maxGroupID < pair[i]) maxGroupID = pair[i];
+		}
 	}
-	delete pair;
-	return maxVal + 1;
+	delete [] pair;
+
+	return maxGroupID + 1;
 }
